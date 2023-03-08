88d528576442ce3fdbebb5df993804bf67feb14b
SteffySenson <steffysenson@gmail.com>
2023-03-08 11:55:14 +0530

    updated file

--- admin/progress/progressadd.php
+++ admin/progress/progressadd.php
@@ -36,7 +36,7 @@ $bname=$_POST['bname'];
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>register form</title>
+   <title>Add</title>
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
